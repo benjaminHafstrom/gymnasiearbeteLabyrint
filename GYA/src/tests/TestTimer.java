af1428118793e8137f96dac835dceb77cae9ed37
benjaminHafstrom <hafstrombenjamin07@gmail.com>
2025-11-25 11:02:58 +0100

    Update TestTimer.java

--- GYA/src/tests/TestTimer.java
+++ GYA/src/tests/TestTimer.java
@@ -1,7 +1,16 @@
 package tests;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class TestTimer {
     public static void main(String[] args) {
         System.out.println("HEJ!");
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(77, 187, 122));
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
