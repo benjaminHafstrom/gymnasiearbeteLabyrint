e4db082691aeb1e81965c01a8627ee58102e66ac
benjaminHafstrom <hafstrombenjamin07@gmail.com>
2025-12-16 10:53:40 +0100

    aaaaaaaaaah

---
+++ GYA/src/tests/LabyrintPlayField.java
@@ -0,0 +1,87 @@
+package tests;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.util.Random;
+
+
+public class LabyrintPlayField extends JPanel {
+    private final int rutorPerRad = 16;
+    private int längdSida = 640/rutorPerRad;
+    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private Random random;
+    private boolean[][] vertikalaVäggar = new boolean[rutorPerRad][rutorPerRad];
+    private boolean[][] horisontellaVäggar = new boolean[rutorPerRad][rutorPerRad];
+
+    public static void main(String[] args) {
+        System.out.println("Height - " + screenSize.height);
+        System.out.println("Width - " + screenSize.width);
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+        JPanel panel = new LabyrintPlayField();
+        panel.setBackground(Color.LIGHT_GRAY);
+        panel.setPreferredSize(new Dimension(640,640));
+        panel.setBorder(new LineBorder(Color.BLACK,2));
+        frame.add(panel);
+        frame.repaint();
+
+        frame.pack();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+
+    }
+
+    public boolean[][] vertikalVäggEllerEj() {
+        for (int i = 0; i < rutorPerRad; i++) {
+            for (int j = 0; j < rutorPerRad; j++) {
+                int a = (int) Math.random()*2;
+                if (a==1) vertikalaVäggar[i][j] = true;
+                if (a==0) vertikalaVäggar[i][j] = false;
+            }
+        }
+
+        return vertikalaVäggar;
+    }
+
+    int a = 0;
+    public boolean[][] horisontellVäggEllerEj() {
+        for (int i = 0; i < rutorPerRad; i++) {
+            for (int j = 0; j < rutorPerRad; j++) {
+                int a = (int) (Math.random()*2);
+                if (a==1) horisontellaVäggar[i][j] = true;
+                if (a==0) horisontellaVäggar[i][j] = false;
+            }
+        }
+        return horisontellaVäggar;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,längdSida,4);
+        for (int j = 0; j < rutorPerRad; j++) {
+            for (int k = 0; k < rutorPerRad; k++) {
+                if (horisontellaVäggar[j][k]==true) {
+                    g.fillRect( (rutorPerRad)*j,((rutorPerRad)*k)*längdSida,4,längdSida);
+                }
+            }
+        }
+        for (int j = 0; j < rutorPerRad; j++) {
+            for (int k = 0; k < rutorPerRad; k++) {
+                if (vertikalaVäggar[j][k]==true) {
+                    g.fillRect( ((rutorPerRad)*j)*längdSida,((rutorPerRad)*k)*längdSida,4,längdSida);
+                }
+
+            }
+        }
+
+
+    }
+
+}
+
--- GYA/src/tests/TestTimer.java
+++ GYA/src/tests/TestTimer.java
@@ -5,11 +5,24 @@ import java.awt.*;
 
 public class TestTimer {
     public static void main(String[] args) {
-        System.out.println("HEJ!");
+        int bredd = 11;
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(77, 187, 122));
-        frame.add(panel);
+        frame.setLayout(new GridLayout(11,11));
+        for (int i = 0; i < 121; i++) {
+            if( i % bredd == (bredd/2) ){
+                JPanel panel = new JPanel();
+                panel.setBackground(new Color(0, 0, 0));
+                frame.add(panel);
+            }
+            else {
+                JPanel panel = new JPanel();
+                panel.setBackground(new Color(77, 187, 122));
+                frame.add(panel);
+
+            }
+        }
+
+        frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
